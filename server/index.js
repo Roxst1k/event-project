444bf450521b9b84eb21ba51fcec8877825567e1
Martiniuk Rostislav <roxst1k@gmail.com>
2024-09-06 17:57:18 +0300

    Change in versel

--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ import {router as eventRoutes} from "./src/routes/event-routes.js";
 
 const PORT = process.env.PORT || 5000;
 const URL = 'mongodb+srv://roxst1k:roxst1k@cluster0.wp3mn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-const corsOptions = {
-    origin: 'http://localhost:3000', // Дозволяє доступ тільки з цього домену
-    optionsSuccessStatus: 200
-};
+// const corsOptions = {
+//     origin: 'http://localhost:3000', // Дозволяє доступ тільки з цього домену
+//     optionsSuccessStatus: 200
+// };
 
 
 const app = express();
