1b5f0d85f6577b2a08f499ad05529321a558ea11
Martiniuk Rostislav <roxst1k@gmail.com>
2024-09-06 18:02:08 +0300

    New index file

--- server/index.js
+++ server/index.js
@@ -3,34 +3,42 @@ import mongoose from "mongoose";
 import cors from "cors";
 import {router as eventRoutes} from "./src/routes/event-routes.js";
 
-const PORT = process.env.PORT || 5000;
-const URL = 'mongodb+srv://roxst1k:roxst1k@cluster0.wp3mn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+// const PORT = process.env.PORT || 5000;
+// const URL = 'mongodb+srv://roxst1k:roxst1k@cluster0.wp3mn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 // const corsOptions = {
 //     origin: 'http://localhost:3000', // Дозволяє доступ тільки з цього домену
 //     optionsSuccessStatus: 200
 // };
+//
+//
+// const app = express();
+//
+// app.use(cors(corsOptions));
+//
+// app.use(express.json());
+// app.use(eventRoutes)
+//
+// mongoose.connect(URL)
+//     .then(() => console.log('Connected to MongoDB'))
+//     .catch((err) => console.log(`Not connect to MongoDB`, err));
+//
+//
+// app.listen(PORT,(err) => {
+//     err ? console.log(err) : console.log('Server running on port', PORT);
+// });
 
 
 const app = express();
-
-// app.use(cors(corsOptions));
-
 app.use(express.json());
-// app.use(eventRoutes)
 
-app.get('/', (req, res) => {
-    res.status(200).send('success');
+app.get("/", async (req, res) => {
+    res.status(200).json({
+        status: "success",
+        text: 'Hello World!'
+    })
 })
 
-mongoose.connect(URL)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.log(`Not connect to MongoDB`, err));
-
-
-app.listen(PORT,(err) => {
-    err ? console.log(err) : console.log('Server running on port', PORT);
-});
-
-
-
+app.listen(5000, (err) => {
+    err ? console.log(err) : console.log('Server running on port 5000.');
+})
 
